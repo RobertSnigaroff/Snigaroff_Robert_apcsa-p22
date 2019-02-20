70d661992a6bc8543ab98a798878be9a69582341
RobertSnigaroff <47190049+RobertSnigaroff@users.noreply.github.com>
2019-02-20 10:20:57 -0800

    Started Unit07

---
+++ Unit07/src/BiggestDouble.java
@@ -0,0 +1,39 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -  
+
+import static java.lang.System.*;
+
+public class BiggestDouble
+{
+	private double one,two,three,four;
+
+	public BiggestDouble()
+	{
+		this(0,0,0,0);
+	}
+
+	public BiggestDouble(double a, double b, double c, double d)
+	{
+		setDoubles(0,0,0,0);
+	}
+
+	public void setDoubles(double a, double b, double c, double d)
+	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
+	}
+
+	public double getBiggest()
+	{
+		if ()
+		return 0.0;
+	}
+
+	public String toString()
+	{
+	   return "";
+	}
+}
